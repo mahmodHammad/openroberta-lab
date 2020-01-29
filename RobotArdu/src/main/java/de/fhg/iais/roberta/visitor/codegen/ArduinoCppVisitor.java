1560d4cb981d7b506f94badd227f761eec93bc8a
Artem Vinokurov <artem.vinokurov@iais.fraunhofer.de>
2020-01-29 14:44:27 +0100

    issue #547 - adds generation of user function prototypes

--- RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/codegen/ArduinoCppVisitor.java
+++ RobotArdu/src/main/java/de/fhg/iais/roberta/visitor/codegen/ArduinoCppVisitor.java
@@ -587,6 +587,8 @@ public final class ArduinoCppVisitor extends AbstractCommonArduinoCppVisitor imp
         }
         this.sb.append("#include <RobertaFunctions/NEPODefs.h>");
         nlIndent();
+        generateSignaturesOfUserDefinedMethods();
+        nlIndent();
 
         super.generateProgramPrefix(withWrapping);
     }
